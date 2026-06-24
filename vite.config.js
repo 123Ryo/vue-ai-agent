9337cbe5dc878387ab699acd886db41465c06d40
roy234578 <t23284g3@gmail.com>
2026-06-24 18:29:23 +0800

    remove github actions and adjust config for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vue/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-ai-agent-dashboard/',
   plugins: [vue()],
   resolve: {
     alias: {
